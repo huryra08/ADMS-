21be57dfa8390f41b5bd096a4c69391ab878f2a8
Abu Huryra <124706375+huryra08@users.noreply.github.com>
2023-05-31 17:26:40 +0600

    Add files via upload

---
+++ ADVANCE DATABASE MANAGEMENT SYSTEM [D]/Bus_Ticket/customer_dashbord.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<meta charset="utf-8">
+    <title>BUS TICKET</title>
+    <style>
+    a:link, a:visited {
+    background-color: #2691d9;
+    border-radius: 25px;
+    color: white;
+    padding: 14px 25px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    }
+
+    a:hover, a:active {
+    background-color: skyblue;
+    }
+
+ body {
+  background-image: url('abc.jpg');
+
+}
+  </style>
+</head>
+<body>
+<a href="index.php">HOME</a>
+<div class="container">
+  <h1>WELCOME</h1>
+</div>
+<div id="mainHolder" style="overflow: auto; max-height: 600px;">
+     
+</body>
+</html>
---
+++ ADVANCE DATABASE MANAGEMENT SYSTEM [D]/Bus_Ticket/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>BUS TICKET</title>
+<style> 
+a:link, a:visited {
+    background-color: #2691d9;
+    border-radius: 25px;
+    color: white;
+    padding: 14px 25px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    }
+
+    a:hover, a:active {
+    background-color: skyblue;
+    }  
+body {
+  background-image: url('12.jpg');
+
+}
+      
+</style>
+</head>
+<body>
+    <a href="index.php">HOME</a>
+    <a href="signin.php">SIGN IN</a>
+    <a href="signup.php">SIGN UP</a>
+</body>
+</html>
